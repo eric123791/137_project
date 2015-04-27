40059f6eb42f85a4150a2788cd055ba5a09a3b98
Edmond Ye <eye1@uci.edu>
2015-04-27 13:31:57 -0700

    buy with menus

--- buy.php
+++ buy.php
@@ -7,6 +7,20 @@
   </head>
 
   <body>
+    <table summary="Our E-Store">
+      <tr>
+        <td colspan="5">
+          <img src="images/logo.png" alt="TicketzHub" />
+        </td>
+      </tr>
+      <tr>
+        <td width="20%"><a href="index.php">Home</a></td>
+        <td width="20%"><a href="buy.php">Buy Now</a></td>
+        <td width="20%"><a href="products.php">Products and Services</a></td>
+        <td width="20%"><a href="yourtickets.php">Your Tickets</a></td>
+        <td width="20%"><a href="about.php">About Us</a></td>
+      </tr>
+    </table>
     <form id = "orderForm" action = "" onsubmit = "processOrder()">
       <fieldset>
         <table>
@@ -54,5 +68,12 @@
         </table>
       </fieldset>
     </form>
+    <table>
+    <tr>
+        <td colspan="3">TicketzHub &copy; 2015 Hua Hsin and Edmond Ye </td>
+        <td><a href="contact.php">Contact Us</a></td>
+        <td><a href="sitemap.php">Site Map</a></td>
+      </tr>
+    </table>
   </body>
 </html>
