698c2bdde71e1c58c4470053c973a2b7bab6c463
Edmond Ye <eye1@uci.edu>
2015-05-04 16:31:52 -0700

    change footername

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,7 @@
 <div id = "footer">
-  <ul id = "footerName">
-    <li>TicketzHub &copy; 2015 Hua Hsin and Edmond Ye</li>
-  </ul>
+  <p id = "footerName">
+    TicketzHub &copy; 2015 Hua Hsin and Edmond Ye
+  </p>
   <ul id="footerLinks">
     <li><a href="contact.php">Contact Us</a></li>
     <li><a href="sitemap.php">Site Map</a></li>
